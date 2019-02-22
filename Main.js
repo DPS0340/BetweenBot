d5b1261750f75ab27f2feb5e6e100d44456c307d
jiho lee <jh001007@gmail.com>
2019-02-22 20:03:34 +0900

    Rename index.js to Main.js

---
+++ Main.js
@@ -0,0 +1,3 @@
+var Discord = require('discord.js');
+var client = new Discord.Client();
+client.login('NTQ3NzQwNjYzNTM0MzIxNjY1.D0-w6g.B6MzTE0UHKLMZeOJQOuZ93FqjMs');
--- index.js
+++
@@ -1,3 +0,0 @@
-var Discord = require('discord.js');
-var client = new Discord.Client();
-client.login('NTQ3NzQwNjYzNTM0MzIxNjY1.D0-w6g.B6MzTE0UHKLMZeOJQOuZ93FqjMs');
