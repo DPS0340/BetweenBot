a79420dcfe2aa0b6e27877fbdbd862aa65b01e11
ttakkku <42809517+ttakkku@users.noreply.github.com>
2019-02-22 23:13:38 +0900

    token

--- Main.js
+++ Main.js
@@ -1,3 +1,4 @@
 var Discord = require('discord.js');
 var client = new Discord.Client();
-client.login('NTQ3NzQwNjYzNTM0MzIxNjY1.D0-w6g.B6MzTE0UHKLMZeOJQOuZ93FqjMs');
+const config = require('./botsetting.json');
+client.login(config.token);
