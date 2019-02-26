1c1225a50b2a0d66ee194428652d740f2b1f7fe1
ttakkku <42809517+ttakkku@users.noreply.github.com>
2019-02-26 10:39:53 +0900

    hex 명령어 추가

--- Main.js
+++ Main.js
@@ -382,6 +382,13 @@ client.on('message', msg => {
            .addField('아이디', role.id);
         msg.channel.send(embed);
        }
+       if (command.startsWith('hex')) {
+        let color = ((1 << 24) * Math.random() | 0).toString(16); 
+        let embed = new Discord.RichEmbed() 
+            .setTitle(`#${color}`)
+            .setColor(`#${color}`);
+         msg.channel.send({ embed: embed });
+       }
     }
 });
 
