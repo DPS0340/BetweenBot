99d5935d93165d9733cf82e9a2f1ed25a503a303
Naesung <42634731+Naesung@users.noreply.github.com>
2019-03-02 21:38:46 +0900

    Update search.js

--- modules/search.js
+++ modules/search.js
@@ -1,1 +1,24 @@
-//making//
+//if (message.content.startsWith(`${prefix}youtube`)) {
+        let youtube = args.slice(0).join('+');
+
+        let link = `https://www.youtube.com/results?search_query=` + youtube;
+        if (!youtube) return message.reply(`Please enter a keyword.`)
+        if (!link) return message.reply("Please enter a keyword, no a link.")
+        let embed = new Discord.RichEmbed()
+
+
+            .setColor("RED")
+
+            .setTimestamp()
+
+            .addField('Action:', 'Searching on youtube')
+
+            .addField("Word:", `${args.slice(0).join(' ')}`)
+
+            .addField('Link:', `${link}`)
+
+            .setFooter("Your avatar", message.author.avatarURL);
+
+        message.channel.send(embed);
+    }
+}//
